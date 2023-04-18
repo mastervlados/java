bb2ef5f887a9bb11a47d89b33ab39430c47b9c70
Vladislav Elizarov <velizarov95@yandex.ru>
2023-04-18 18:25:45 -0400

    Complete Homework 1

---
+++ 002/VendingMachine/src/Products/HotDrink.java
@@ -0,0 +1,34 @@
+package Products;
+
+public class HotDrink extends Product {
+    
+    /** Температура напитка */
+    private int temperature;
+
+    public HotDrink(String name, double price, int temperature)
+    {
+        super(name, price);
+        this.temperature = temperature;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    /**
+     * переопределение вывода продукта
+     */
+    @Override
+    public String toString()
+    {
+        return  "Product{" +
+        "name='" + super.getName() + '\'' +
+        ", cost=" + super.getPrice() +
+        ", temperature=" + temperature +
+        "C}";
+    }
+}
