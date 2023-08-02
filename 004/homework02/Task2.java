7345fcc635c72d99933ef308dc5a6e3a888f0f91
Vladislav Elizarov <velizarovdev@gmail.com>
2023-08-02 13:59:19 +0300

    Complete Homework 2

--- 004/homework02/Task2.java
+++ 004/homework02/Task2.java
@@ -7,10 +7,12 @@ public class Task2 {
 
         try {
             int d = 0;
-            double catchedRes1 = intArray[8] / d;
+            double catchedRes1 = intArray[6] / d;
             System.out.println("catchedRes1 = " + catchedRes1);
         } catch (ArithmeticException e) {
-            System.out.println("Catching exception: " + e);
-        }     
+            System.err.println("Arithmetic exception: " + e);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("Array exception: " + e);
+        }   
     }
 }
--- 004/homework02/Task3.java
+++ 004/homework02/Task3.java
@@ -1,7 +1,5 @@
 package homework02;
 
-import java.io.FileNotFoundException;
-
 public class Task3 {
     public static void main(String[] args) throws Exception {
 
@@ -12,16 +10,16 @@ public class Task3 {
             printSum(23, 234);
             int[] abc = { 1, 2 };
             abc[3] = 9;
-        } catch (Throwable ex) {
-            System.out.println("Что-то пошло не так...");
         } catch (NullPointerException ex) {
             System.out.println("Указатель не может указывать на null!");
         } catch (IndexOutOfBoundsException ex) {
             System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
         }
     }
-    
-    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+
+    public static void printSum(Integer a, Integer b) throws ArithmeticException {
         System.out.println(a + b);
     }
 }
